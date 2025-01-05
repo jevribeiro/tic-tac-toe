11570417df0ffd52780deda2abf8bcc49c0e2260
João Ribeiro <191224865+jevribeiro@users.noreply.github.com>
2025-01-05 06:34:44 +0000

    Create and print board

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        char[][] board = {
+                {'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'}};
+
+        printGameBoard(board);
+
+    }
+
+    private static void printGameBoard(char[][] board) {
+        System.out.println(board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
+        System.out.println("---------");
+        System.out.println(board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
+        System.out.println("---------");
+        System.out.println(board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
     }
 }
